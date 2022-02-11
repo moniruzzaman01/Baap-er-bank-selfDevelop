4ba25c4b72f6f60b55a4e8c6ab31cc5a426f5f0c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-02-12 00:19:21 +0600

    buttons validation added

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,4 @@
-// handle deposit and withdraw
+// deposit and withdraw amount handle
 function handleDepositAndWithdraw(totalAmountField, InputAmountField, isDeposit) {
     // get Total Amount field
     const amountTotalField = document.getElementById(totalAmountField)
@@ -13,18 +13,32 @@ function handleDepositAndWithdraw(totalAmountField, InputAmountField, isDeposit)
     // clear input field
     amountInputField.value = '';
 }
-// handle balance
+// balance amount handle
 function handleBalance(InputAmountField, isDeposit) {
     const balanceField = document.getElementById('balance')
     const previousBalance = parseInt(balanceField.innerText)
-    const depositInputField = document.getElementById(InputAmountField)
-    const newDeposit = parseInt(depositInputField.value)
+    const amountInputField = document.getElementById(InputAmountField)
+    const amountInput = parseInt(amountInputField.value)
     if (isDeposit == true) {
-        const newBalance = previousBalance + newDeposit
+        const newBalance = previousBalance + amountInput
         balanceField.innerText = newBalance
     } else {
-        const newBalance = previousBalance - newDeposit
+        const newBalance = previousBalance - amountInput
         balanceField.innerText = newBalance
+    }
+}
+// button's work handle
+function btnValidation(buttonId, inputId) {
+    const targetInputBtn = document.getElementById(buttonId)
+    const targetInputField = document.getElementById(inputId)
+    if (targetInputField.value > 0) {
+        targetInputBtn.removeAttribute('disabled')
+        targetInputBtn.classList.remove('bg-red-200')
+        targetInputBtn.classList.add('bg-red-400')
+    } else {
+        targetInputBtn.setAttribute('disabled', true)
+        targetInputBtn.classList.remove('bg-red-400')
+        targetInputBtn.classList.add('bg-red-200')
     }
 }
 
@@ -32,10 +46,18 @@ function handleBalance(InputAmountField, isDeposit) {
 // for Deposit Field
 document.getElementById('deposit-btn').addEventListener('click', function () {
     handleDepositAndWithdraw('deposit-total', 'deposit-input', true)
+    btnValidation('deposit-btn', 'deposit-input')
+})
+document.getElementById('deposit-input').addEventListener('keyup', function () {
+    btnValidation('deposit-btn', 'deposit-input')
 })
 
 
 // For Withdraw Field
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     handleDepositAndWithdraw('withdraw-total', 'withdraw-input', false)
+    btnValidation('withdraw-btn', 'withdraw-input')
+})
+document.getElementById('withdraw-input').addEventListener('keyup', function () {
+    btnValidation('withdraw-btn', 'withdraw-input')
 })
